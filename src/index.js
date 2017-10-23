4448438065e362b9599717f606bf4d4c7e775d5c
Harshit Agarwal <harshit_agrawal36@yahoo.com>
2017-10-23 00:00:16 -0700

    adding style

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyBOSmKNexx-kHG4fVLKXG33e5WfLyeriPc';
+import YTSearch from 'youtube-api-search';
 
-const App = () => {
-  return(
-    <div>
-    <SearchBar></SearchBar>
-  </div>
-  );
-};
+
+
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+
+    YTSearch({key:API_KEY, term: 'flicker'} , (videos) => {
+          this.setState({ videos});
+          //which is this.setState({ videos: videos});
+        });
+  }
+  render() {
+    return(
+      <div>
+      <SearchBar></SearchBar>
+    </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
