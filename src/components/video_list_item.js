998f00ee7041e6e378cf3c775dba8538b90225ba
Harshit Agarwal <harshit_agrawal36@yahoo.com>
2017-10-25 20:19:18 -0700

    React only Youtube Like web APP

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   // const video = props.video;
 const imgURL = video.snippet.thumbnails.default.url;
   return (
-    <li className="list-group-item">
+    <li onClick={ () => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img src={imgURL} alt="" className="media-object"/>
--- src/index.js
+++ src/index.js
@@ -5,24 +5,39 @@ const API_KEY = 'AIzaSyBOSmKNexx-kHG4fVLKXG33e5WfLyeriPc';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/Video_List';
 import VideoDetail from './components/Video_Detail';
+import _ from 'lodash';
 
 
 class App extends Component{
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+     };
+     this.videoSearch('flicker');
+  }
 
-    YTSearch({key:API_KEY, term: 'flicker'} , (videos_data) => {
-          this.setState({ videos: videos_data});
+  videoSearch(term) {
+    YTSearch({key:API_KEY, term: term} , (videos_data) => {
+          this.setState({
+            videos: videos_data,
+            selectedVideo: videos_data[0],
+          });
           //which is this.setState({ videos: videos}); only works key name==property name
         });
   }
+
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
     return(
       <div>
-      <SearchBar/>
-      <VideoDetail video={this.state.videos[0]} />
-      <VideoList videos={this.state.videos}/>
+      <SearchBar onSearchTermChange= {videoSearch}/>
+      <VideoDetail video={this.state.selectedVideo} />
+      <VideoList
+        videos={this.state.videos}
+        onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+      />
     </div>
     );
   }
